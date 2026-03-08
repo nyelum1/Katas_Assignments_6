c7bc64bf481838611ab58fbaed0dca7bc09ea540
Nkeonyelu Igboanugo <nigboanugo1@buffs.wtamu.edu>
2026-03-08 15:47:02 -0500

    function for creating synthetic data

--- katas6.py
+++ katas6.py
@@ -7,3 +7,11 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 # --- CONFIGURATION ---
 NUM_ROWS = 1_000_000
 NUM_CHUNKS = 10
+
+
+# --- 1. DATA PREP ---
+def get_dataset():
+    """Generates or reads the dataset."""
+    print(f"Generating {NUM_ROWS:,} rows of data...")
+    df = pd.DataFrame(np.random.randn(NUM_ROWS, 1), columns=['value'])
+    return np.array_split(df, NUM_CHUNKS)
